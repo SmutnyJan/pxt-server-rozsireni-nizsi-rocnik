d10d4b1283b1e2d690b6b46b67c9979a12d55a95
SmutnyJan <57211415+SmutnyJan@users.noreply.github.com>
2021-12-10 00:06:53 +0100

    initial changes

--- main.ts
+++ main.ts
@@ -1,1 +1,13 @@
-
+input.onButtonPressed(Button.A, function () {
+    for (let item of answers) {
+        basic.showString("" + (item))
+    }
+})
+radio.onReceivedString(function (receivedString) {
+    answers.push(receivedString)
+})
+let answers: string[] = []
+radio.setGroup(1)
+basic.forever(function () {
+	
+})
