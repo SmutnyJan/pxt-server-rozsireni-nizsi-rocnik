4a4a64dc14ced0f1e06ae0a97f14f3d3299c1997
SmutnyJan <57211415+SmutnyJan@users.noreply.github.com>
2021-12-11 15:49:05 +0100

    traffic handling based on serial number

--- main.ts
+++ main.ts
@@ -3,9 +3,26 @@ input.onButtonPressed(Button.A, function () {
         basic.showString("" + (item))
     }
 })
-radio.onReceivedString(function (receivedString) {
-    answers.push(receivedString)
+input.onButtonPressed(Button.B, function () {
+    answers = []
+    serialNumbers = []
 })
+radio.onReceivedValue(function (name, value) {
+    contains = false
+    for (let number of serialNumbers) {
+        if (number == value) {
+            contains = true
+        }
+    }
+    if (contains == false) {
+        answers.push(name)
+        serialNumbers.push(value)
+    } else {
+        answers[serialNumbers.indexOf(value)] = name
+    }
+})
+let contains = false
+let serialNumbers: number[] = []
 let answers: string[] = []
 radio.setGroup(1)
 basic.forever(function () {
