f6dca99a8229c955d678350a5843b39746d75fd1
SmutnyJan <57211415+SmutnyJan@users.noreply.github.com>
2021-12-27 21:58:08 +0100

    improved displaying of values

--- main.ts
+++ main.ts
@@ -1,7 +1,29 @@
+function prevestOdpoved (text: string) {
+	switch (text) {
+        case "A":
+            Acount++
+            break
+        case "B":
+            Bcount++
+            break
+        case "C":
+            Ccount++
+            break
+        case "D":
+            Dcount++
+            break
+    }
+}
 input.onButtonPressed(Button.A, function () {
     for (let item of answers) {
-        basic.showString("" + (item))
+        prevestOdpoved(item)       
     }
+
+    basic.showString("" + A + " " + Acount)
+    basic.showString("" + B + " " + Bcount)
+    basic.showString("" + C + " " + Ccount)
+    basic.showString("" + D + " " + Dcount)
+
 })
 input.onButtonPressed(Button.B, function () {
     answers = []
@@ -24,6 +46,14 @@ radio.onReceivedValue(function (name, value) {
 let contains = false
 let serialNumbers: number[] = []
 let answers: string[] = []
+let A = "ANO"
+let B = "NE"
+let C = "MOZNA"
+let D = "NEVIM"
+let Acount = 0
+let Bcount = 0
+let Ccount = 0
+let Dcount = 0
 radio.setGroup(1)
 basic.forever(function () {
 	
